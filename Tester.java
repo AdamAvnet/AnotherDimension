3a22ad28f2d7533b6ba875adcf0638db4221042e
AdamAvnet <aavnet20@stuy.edu>
2020-10-22 14:38:47 -0400

    Final draft of sum test

--- Tester.java
+++ Tester.java
@@ -4,10 +4,15 @@ public class Tester{
 	
 	// sum test:
 
-	int[] sumtest = new int[5];
-	sumtest = new int[] {4, 5, 2, 3, 7};
-		
-	System.out.println(ArrayOps.sum(sumtest));
-
+		int[] sumtest = new int[5];
+		for(int i = 1; i < 4; i++){
+			if(i == 1)
+				sumtest = new int[] {4, 5, 2, 3, 7};
+			if(i == 2)
+				sumtest = new int[] {-3, 5, 2, -7};
+			if(i == 3)
+				sumtest = new int[] {0, 4, 7, 8, 6};
+			System.out.println(ArrayOps.sum(sumtest));
+		}
 	}
 }
