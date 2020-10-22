2f29ba34180ed1ba135a0a0852bed0dfd9fa4e5e
AdamAvnet <aavnet20@stuy.edu>
2020-10-22 13:32:52 -0400

    First draft of sum function

---
+++ Tester.java
@@ -0,0 +1,13 @@
+public class Tester{
+	
+	public static void main(String[] args){
+	
+	// sum test:
+
+	int[] sumtest = new int[5];
+	sumtest = new int[] {4, 5, 2, 3, 7};
+		
+	System.out.println(ArrayOps.sum(sumtest));
+
+	}
+}
